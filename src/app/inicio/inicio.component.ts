76cc0cd3c50426574ea762b3a60db850f90ade5f
Wilker <wilkerjim011@gmail.com>
2023-10-27 18:27:05 -0400

    1.0.2

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  fadeInAnimation() {
+    const observable = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('.hideAnimation');
+          entry.target.classList.add('.showAnimation');
+        }
+      })
+    })
+    const classType = document.querySelectorAll('.hideAnimation');
+    classType.forEach((e) => observable.observe(e))
+    console.log("done")
+  }
+
 
 }
