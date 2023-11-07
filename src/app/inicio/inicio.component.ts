764f837a1e112827ddf976b2dda337096cb468c8
Wilker <wilkerjim011@gmail.com>
2023-11-07 19:20:47 -0400

    1.3.1

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -33,12 +33,32 @@ export class InicioComponent implements OnInit {
     var target = event.srcElement.attributes.id || event.currentTarget.id;
     var link = document.getElementById("scroll_" + target);
     console.log(link)
+    this.navMenu(false);
     link?.scrollIntoView({ behavior: "smooth" })
 
   }
 
-  redirect(url:string){
+  redirect(url: string) {
     window.open(url, '_blank');
+  }
+
+  navMenu(value: boolean) {
+    let body = document.getElementById('body');
+    let navegation = document.getElementById('cellNav');
+    let btn = document.getElementById('menuBtn');
+    if (value === true) {
+      btn?.classList.remove("-rotate-0")
+      btn?.classList.add("-rotate-90")
+      body?.classList.add("fixed")
+      navegation?.classList.remove("hidden")
+      navegation?.classList.add("cellNavAnimation")
+    } else {
+      btn?.classList.remove("-rotate-90")
+      btn?.classList.add("-rotate-0")
+      body?.classList.remove("fixed")
+      navegation?.classList.remove("cellNavAnimation")
+      navegation?.classList.add("hidden")
+    }
   }
 
 
